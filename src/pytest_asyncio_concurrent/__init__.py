d839936f45c79eb5b29bc0c3f1e680fd16af7ca3
zane <czl970721@gmail.com>
2024-12-22 14:21:04 -0500

    lint

--- src/pytest_asyncio_concurrent/__init__.py
+++ src/pytest_asyncio_concurrent/__init__.py
@@ -1,6 +1,8 @@
 """The main point for importing pytest-asyncio-concurrent items."""
 
 from typing import List
-from .plugin import *
+from .plugin import AsyncioConcurrentGroup
 
-__all__: List[str] = []
+__all__: List[str] = [
+    AsyncioConcurrentGroup.__name__,
+]
