9ec63c5e5bf0b82c051a3bd8e6d778ea365a3a1e
zane <czl970721@gmail.com>
2024-12-25 15:16:54 -0500

    test cover setup tear down error

--- tests/test_status.py
+++ tests/test_status.py
@@ -26,6 +26,34 @@ def test_fail(pytester: pytest.Pytester):
     result.assert_outcomes(failed=1, passed=1)
 
 
+def test_fail_multi(pytester: pytest.Pytester):
+    """Make sure tests failed is reported correctly"""
+
+    pytester.makepyfile(
+        dedent(
+            """\
+            import asyncio
+            import pytest
+
+            @pytest.mark.asyncio_concurrent(group="any")
+            async def test_passing():
+                pass
+
+            @pytest.mark.asyncio_concurrent(group="any")
+            async def test_failedA():
+                raise AssertionError
+
+            @pytest.mark.asyncio_concurrent(group="any")
+            async def test_failedB():
+                raise AssertionError
+            """
+        )
+    )
+
+    result = pytester.runpytest()
+    result.assert_outcomes(failed=2, passed=1)
+
+
 def test_skip(pytester: pytest.Pytester):
     """Make sure tests skip is reported correctly"""
 
@@ -105,3 +133,83 @@ def test_xfail_xpass(pytester: pytest.Pytester):
 
     result = pytester.runpytest()
     result.assert_outcomes(failed=1, xfailed=1, xpassed=1)
+
+
+def test_selection(pytester: pytest.Pytester):
+    """Make sure tests xfail and xpass is reported correctly"""
+
+    pytester.makepyfile(
+        dedent(
+            """\
+            import asyncio
+            import pytest
+
+            @pytest.mark.asyncio_concurrent(group="any")
+            async def test_selectedA():
+                pass
+
+            @pytest.mark.asyncio_concurrent(group="any")
+            async def test_selectedB():
+                pass
+
+            @pytest.mark.asyncio_concurrent(group="any")
+            async def test_unselected():
+                pass
+            """
+        )
+    )
+
+    result = pytester.runpytest("-k test_selected")
+    result.assert_outcomes(deselected=1, passed=2)
+
+
+def test_setup_error(pytester: pytest.Pytester):
+    """Make sure tests xfail and xpass is reported correctly"""
+
+    pytester.makepyfile(
+        dedent(
+            """\
+            import asyncio
+            import pytest
+
+            @pytest.fixture(scope="function", name="setup_error_fixture")
+            def function_scoped_fiture_error_in_setup():
+                raise AssertionError
+                yield
+
+            @pytest.mark.asyncio_concurrent(group="any")
+            async def test_xpass(setup_error_fixture):
+                pass
+
+            """
+        )
+    )
+
+    result = pytester.runpytest()
+    result.assert_outcomes(errors=1)
+
+
+def test_teardown_error(pytester: pytest.Pytester):
+    """Make sure tests xfail and xpass is reported correctly"""
+
+    pytester.makepyfile(
+        dedent(
+            """\
+            import asyncio
+            import pytest
+
+            @pytest.fixture(scope="function", name="teardown_error_fixture")
+            def function_scoped_fiture_error_in_teardown():
+                yield
+                raise AssertionError
+
+            @pytest.mark.asyncio_concurrent(group="any")
+            async def test_xpass(teardown_error_fixture):
+                pass
+
+            """
+        )
+    )
+
+    result = pytester.runpytest()
+    result.assert_outcomes(errors=1, passed=1)
