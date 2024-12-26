8b64ec072ef091899cb65097932057cd96d86ccf
zane <czl970721@gmail.com>
2024-12-25 23:20:00 -0500

    more test

--- tests/test_async_fixture.py
+++ tests/test_async_fixture.py
@@ -108,3 +108,33 @@ def test_async_gen_fixture_sync(pytester: pytest.Pytester):
     result = pytester.runpytest()
 
     result.assert_outcomes(passed=1)
+
+
+def test_async_gen_fixture_error(pytester: pytest.Pytester):
+    """
+    Make sure that async generator fixture is got wrapped up
+    and do not allow multiple yield
+    """
+
+    pytester.makepyfile(
+        dedent(
+            """\
+            import asyncio
+            import pytest
+
+            @pytest.fixture(scope="function")
+            async def async_fixture_gen():
+                await asyncio.sleep(0.1)
+                yield 1
+                yield 1
+
+            def test_fixture_async(async_fixture_gen):
+                assert async_fixture_gen == 1
+            """
+        )
+    )
+
+    result = pytester.runpytest()
+
+    result.assert_outcomes(passed=1, errors=1)
+
